0eb7449063588551f6d4a630220c371a948ad51b
breadandsmiles <46297171+breadandsmiles@users.noreply.github.com>
2019-01-10 02:28:35 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,4 +2,4 @@ exports.TOKEN = 'process.env.BOT_TOKEN';
 
 exports.PREFIX = '_';
 
-exports.GOOGLE_API_KEY = 'process.env.GOOGLE_API_KEY';
+exports.GOOGLE_API_KEY = 'AIzaSyDs6QOfIFNVJciHmjfXA2QZ75O0P6sL0X0';
